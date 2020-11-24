b00ebb0337ac35fe75d05b9b85ca10bc502cc830
takamoto0609 <takamoto0609@gamil.com>
2020-11-24 13:17:48 +0900

    1userが1walletを持てるように記述

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :get_new_wallet
+
+  private
+
+  def get_new_wallet
+    if user_signed_in?
+      wallet = Wallet.find_by(user_id: current_user.id)
+      if wallet == nil
+        new_wallet = Wallet.new
+        new_wallet.user_id = current_user.id
+        new_wallet.save
+      end
+    end
+  end
 end
