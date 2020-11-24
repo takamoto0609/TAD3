54c5474b628ac8f4aad7e324d558a01afb694557
takamoto0609 <takamoto0609@gamil.com>
2020-11-24 18:39:12 +0900

    チャット機能の不具合を修正

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,8 +1,9 @@
 class ChatsController < ApplicationController
   # binding.pry
 
-  def index
+  def new
     if user_signed_in?
+      @chat = Chat.new
       @item = Item.find_by(id: params[:item_id])
       @chats = Chat.where(item_id: params[:item_id])
     else
@@ -10,22 +11,18 @@ class ChatsController < ApplicationController
     end
   end
 
-  def new
-    @chat = Chat.new
-  end
-
   def create
-    if params[:message]=""
-      redirect_to item_chats_path(params[:item_id])
+    if Chat.new(chat_params).valid?
+      Chat.create(chat_params)
+      redirect_to new_item_chat_path(params[:item_id])
     else
-      chat = Chat.create(chat_params)
-      redirect_to item_chats_path(params[:item_id])
+      redirect_to new_item_chat_path(params[:item_id])
     end
   end
 
   private
 
   def chat_params
-    params.permit(:message).merge(user_id: current_user.id).merge(item_id: params[:item_id].to_i)
+    params.require(:chat).permit(:message).merge(user_id: current_user.id).merge(item_id: params[:item_id])
   end
 end
